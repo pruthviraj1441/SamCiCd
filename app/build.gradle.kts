660f943e08ff4baca1e67c6f71aeb1b3eba21c94
pruthvirajcp <clef2k22@gmail.com>
2024-10-09 17:58:12 +0530

    Ibkart internal release using cicd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("D:/jks files/cicd/ibkart.jks")
+            storeFile = file("D:/jks files/production/ibkart.jks")
             storePassword = "ibkart@msspay"
             keyAlias = "ibkart"
             keyPassword = "ibkart@msspay"
